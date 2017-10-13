fe0c325f67f49d67ab3efa583c015106e3442e6e
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-10-13 14:43:03 -0400

    AMadon w/bootstrap

---
+++ Django_intro/Amadon/apps/store/products.py
@@ -0,0 +1,37 @@
+items = [
+    {
+        "id": 1,
+        "name": "Mule T-Shirt",
+        "price": 19.99,
+    },
+    {
+        "id": 2,
+        "name": "Wool Sweater",
+        "price": 29.99
+    },
+    {
+        "id": 3,
+        "name": "Thermal Cup",
+        "price": 4.99
+    },
+    {
+        "id": 4,
+        "name": "Algorithm Book",
+        "price": 49.99
+    },
+    {
+        "id": 5,
+        "name": "Running Shoes",
+        "price": 69.99
+    },
+    {
+        "id": 6,
+        "name": "Shorts",
+        "price": 26.99
+    },
+    {
+        "id": 7,
+        "name": "Sun Glasses",
+        "price": 9.99
+    }
+]
