af3bca2b97801e12d52f835c534e67dbacf20615
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-10-14 13:47:51 -0400

    wrapped up loose ends on app

--- Django_intro/djangoApp/djangoApp/urls.py
+++ Django_intro/djangoApp/djangoApp/urls.py
@@ -18,5 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('apps.blogs.urls')),
+    url(r'^blogs/', include('apps.blogs.urls')),
+    url(r'^surveys/', include('apps.surveys.urls')),
+    url(r'^users/', include('apps.users.urls')),
     url(r'^admin/', admin.site.urls),
 ]
