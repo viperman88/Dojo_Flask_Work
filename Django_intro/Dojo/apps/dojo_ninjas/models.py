62b2207f96b2f36d1c18f4429208f52cce338a73
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-10-15 19:40:34 -0400

    initial commit

---
+++ Django_intro/Dojo/apps/dojo_ninjas/models.py
@@ -0,0 +1,18 @@
+# Inside models.py
+from __future__ import unicode_literals
+from django.db import models
+# Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojo, related_name="ninjas")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
